88d1d301f52afd9052f31ebb0772c236b0e75b7d
zhangyi1015 <zycool0828@163.com>
2020-05-08 21:23:08 +0800

    基于注解的Hello_SpringMVC

--- Hello_SpringMVC/src/main/java/com/zhy/controller/HelloController.java
+++ Hello_SpringMVC/src/main/java/com/zhy/controller/HelloController.java
@@ -1,8 +1,10 @@
 package com.zhy.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,9 +15,10 @@ import javax.servlet.http.HttpServletResponse;
  * @Date: 2020/5/8 15:01
  * @Version: 1.0
  **/
-public class HelloController implements Controller {
+@Controller
+public class HelloController {
 
-    @Override
+    /*@Override
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         ModelAndView mv  = new ModelAndView();
         //将需要的值传递到model中
@@ -23,5 +26,17 @@ public class HelloController implements Controller {
         //设置要跳转的视图，
         mv.setViewName("hello");
         return mv;
+    }*/
+
+
+    /*
+     * @RequestMapping就是用来标识此方法用来处理什么请求，其中的/可以取消
+     * 取消后默认也是从当前项目的根目录开始查找，一般在编写的时候看个人习惯
+     * 同时，@RequestMapping也可以用来加在类上，
+     * */
+    @RequestMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("msg","hello,SpringMVC");
+        return "hello";
     }
 }
